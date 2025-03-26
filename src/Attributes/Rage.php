3a25dd844eca5df77178d2ed99b007197eee9cb1
Jordan <jordancasey5278@gmail.com>
2025-03-26 17:56:30 +0700

    refactor examples

--- src/Attributes/Rage.php
+++ src/Attributes/Rage.php
@@ -6,40 +6,71 @@ use Videogame\Interfaces\Attributable;
 use Videogame\Interfaces\Attribute;
 use Videogame\Interfaces\Combat;
 
-class Rage implements Attribute {
-
+class Rage implements Attribute
+{
+    const EXPIRY_COUNT = -3;
     protected $object;
     protected $expires;
     protected $rage_damage_bonus;
+    protected $reduce_health_multiplier = 0.9;
+    protected $damage_multiplier = 0.2;
 
-    public function __construct() {
-        $this->expires = 5;
+    public function __construct($expiry = 5)
+    {
+        $this->expires = $expiry;
     }
 
-    public function affect(Attributable $object) {
-        if ($object instanceof Combat) {
-            if (!$this->expired()) {
-                $current_damage = $object->getDamage();
-                $this->rage_damage_bonus = $current_damage * 0.2;
-                $object->setDamage($current_damage + $this->rage_damage_bonus);
-                $this->expires--;
-            } else {
-                $object->setHealth($object->getHealth() * 0.9);
-                $object->setDamage($object->getDamage() - $this->rage_damage_bonus);
-            }
+    public function affect(Attributable $object)
+    {
+        if (!$object instanceof Combat) {
+            return false;
         }
+
+        if (!$this->expired()) {
+            $this->damageBoost($object);
+        } else {
+            $this->reduceHealth($object)->reduceDamage($object);
+        }
+
+        $this->expires--;
+    }
+
+    private function reduceDamage($object)
+    {
+        $object->setDamage($object->getDamage() - $this->rage_damage_bonus);
+        return $this;
+    }
+
+    private function reduceHealth($object)
+    {
+        $object->setHealth($object->getHealth() * $this->reduce_health_multiplier);
+        return $this;
+    }
+
+    /**
+     * Applies a damage boost
+     */
+    private function damageBoost($object)
+    {
+        $current_damage = $object->getDamage();
+        $this->rage_damage_bonus = $current_damage * $this->damage_multiplier;
+        $object->setDamage($current_damage + $this->rage_damage_bonus);
+        return $object;
     }
 
 
-    public function getName(): string {
+    public function getName(): string
+    {
         return 'rage';
     }
 
-    public function expired(): bool {
-        return $this->expires <= 0;
+    public function expired(): bool
+    {
+        return $this->expires <= self::EXPIRY_COUNT;
     }
 
-    public function setExpiry($expire):void{
+    public function setExpiry($expire): void
+    {
         $this->expires = $expire;
     }
 }
--- src/Attributes/Weak.php
+++ src/Attributes/Weak.php
@@ -6,36 +6,51 @@ use Videogame\Interfaces\Attributable;
 use Videogame\Interfaces\Attribute;
 use Videogame\Interfaces\Combat;
 
-class Weak implements Attribute {
+class Weak implements Attribute
+{
     protected $expires;
     protected $damage_reduced;
+    protected $reduce_damage_multiplier = 0.2;
 
-    public function __construct($expiration) {
+    public function __construct($expiration)
+    {
         $this->expires = $expiration;
     }
 
-    public function affect(Attributable $object) {
-        if ($object instanceof Combat) {
-            if (!$this->expired()) {
-                $current_damage = $object->getDamage();
-                $this->damage_reduced = $current_damage * 0.2;
-                $object->setDamage($current_damage - $this->damage_reduced);
-                $this->expires--;
-            } else {
-                $object->setDamage($object->getDamage() + $this->damage_reduced);
-            }
+    public function affect(Attributable $object)
+    {
+        if (!$object instanceof Combat) {
+            return false;
         }
+
+        !$this->expired() ? $this->reduceDamage($object) : $this->increaseDamage($object);
+    }
+
+    private function increaseDamage($object)
+    {
+        $object->setDamage($object->getDamage() + $this->damage_reduced);
+    }
+
+    private function reduceDamage($object)
+    {
+        $current_damage = $object->getDamage();
+        $this->damage_reduced = $current_damage * $this->reduce_damage_multiplier;
+        $object->setDamage($current_damage - $this->damage_reduced);
+        $this->expires--;
     }
 
 
-    public function getName(): string {
+    public function getName(): string
+    {
         return 'weakened';
     }
 
-    public function expired(): bool {
+    public function expired(): bool
+    {
         return $this->expires <= 0;
     }
-    public function setExpiry($expire):void{
+    public function setExpiry($expire): void
+    {
         $this->expires = $expire;
     }
 }
